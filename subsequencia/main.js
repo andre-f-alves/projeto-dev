47f0352a5c575a0e174ec66a96b7f56e215742f5
André Alves <112015245+andre-f-alves@users.noreply.github.com>
2022-10-12 10:17:54 -0300

    Documentação de main.js

--- subsequencia/main.js
+++ subsequencia/main.js
@@ -20,74 +20,116 @@ var resultado = document.querySelector('div#result');
 
 var listaDePalavras = [];
 
-/* 
+/*
     Área de funções
 */
 
-const isValid = valor => valor != '' ? true : false; // -> Varifica se o valor não é uma string vazia
+const isValid = valor => valor != '' ? true : false; // Verifica se um valor não é uma string vazia
 
-const notInList = (item, array) => array.indexOf(item) == -1 ? true : false; // -> Analisa se o valor existe na lista
+const notInList = (item, array) => array.indexOf(item) == -1 ? true : false; // Analisa se um valor existe dentro de um array
 
-function adicionar() { // -> Função para adiconar os valores à lista
+function adicionar() {
+    /*
+    Função acionada com o evento 'onclick'.
     
-    if (isValid(principal.value) && isValid(palavra.value) && notInList(palavra.value, listaDePalavras)) { // -> Valida a palavra
+    Valida os valores inseridos nos campos 'input#principal' e 'input#subsequence', verificando
+    se estes não são strings vazias e se o valor de 'input#subsequence' não está na lista.
+    
+    Após a validação, caso o resultado seja 'true', o valor de 'input#subsequence' é adicionado à lista
+    e exibido na lista de seleção ('select#lista') por meio da criação e adição de um elemento '<option>'
+    ao '<select>' para cada valor inserido.
+
+    Caso a validação resulte em 'false', um alerta de erro é disparado.
+    */
+
+    if (isValid(principal.value) && isValid(palavra.value) && notInList(palavra.value, listaDePalavras)) {
 
         let str = String(palavra.value);
-        listaDePalavras.push(str); // -> Adiciona a palavra a um array
+        listaDePalavras.push(str);
 
-        let item = document.createElement('option'); // -> Cria o elemento '<option>' no documento HTML
+        let item = document.createElement('option');
 
-        for (let index in listaDePalavras) { // -> Adiciona o índice do item como valor ao atributo 'value' de '<option>'
+        for (let index in listaDePalavras) {
+            /* 
+            Percorre pelos índices do array 'listaDePalavras', atribuindo à propriedade 'value' do elemento
+            HTML '<option>' o índice de cada palavra do array precedido de "item".
+            */
             item.value = `item${index}`;
         };
 
         item.text = palavra.value;
-        lista.appendChild(item); // -> Adiciona a tag '<option>' ao '<select>'
+        lista.appendChild(item);
 
         resultado.innerHTML = '';
 
-    } else { // -> Dispara um alerta em caso de erro
+    } else {
         alert('[ERRO] Valor inválido ou já encontrado na lista!');
     };
 
-    palavra.value = ''; // -> Limpa o campo para digitação
-    palavra.focus(); // -> Foca o cursor no campo
+    palavra.value = '';
+    palavra.focus();
 };
 
-function analisar() { // -> Função para analisar se as palavras indicadas são subsequências da palavra principal
+function analisar() {
+    /*
+    Função acionada com o evento 'onclick'.
+    
+    Analisa as palavras inseridas no campo 'input#subsequence', verificando se estas são subsequências
+    da palavra principal, inserida no campo 'input#principal'.
 
-    let mapaStr = mapString(principal.value); // -> Cria um "mapa" da palavra principal
+    Para realizar a análise, um "mapa", ou seja, um objeto (Object), é criado com base na palavra principal.
+    Cada letra da palavra é transformada em uma propriedade do objeto e, como valor para a propriedade,
+    é atribuído um array com o índice, ou posição, em que a letra se encontra*.
+
+    Após a criação do mapa, cada palavra do array 'listaDePalavras' é comparada com os valores das propriedades
+    do objeto criado, verificando se o índice das letras da palavra são menores ou iguais aos índices da palavra
+    principal, indicando se a palavra é ou não uma subseqência da palavra principal. Caso o resultado seja 'true'
+    a palavra é adicionada no array 'listaSeubsequencia'.
+
+    Uma vez concluído todo o processo de análise, é realizada uma validação que indica se o array 'listaSubsequencia'
+    possui valores dentro de si, ou seja, se não é um array vazio. Caso a validação resulte em 'true', é feita uma
+    pesquisa no array 'listaSubsequencia' com o objetivo de capturar a subsequência mais longa e os resultados são
+    exibidos. Caso a validação resulte em 'false', uma mensagem secundária é exibida ao usuário.
+
+    =-=-=-=-=-=
+    * Em casos de mais de uma ocorrência de uma mesma letra na palavra, haverá apenas uma propriedade a referenciando,
+    mas com os índices relativos à sua ocorrência na palavra, isto é, somente seus índices serão adicinados no array.
+    */
+
+    let mapaStr = mapString(principal.value);
     let listaSubsequencia = [];
 
-    for (let palavra of listaDePalavras) { // -> Percorre todas as palavras da lista de possíveis subsequências
-        
-        if (isSubsequence(palavra, mapaStr)) { // -> Verifica se a palavra é uma subsequência
-            
-            listaSubsequencia.push(palavra); // -> Adiciona a palavra à lista de subsequências
+    for (let palavra of listaDePalavras) {
+
+        if (isSubsequence(palavra, mapaStr)) {
+            listaSubsequencia.push(palavra);
         };
     };
 
-    if (listaSubsequencia.length) { // -> Verifica se existem palavras dentro do array
+    if (listaSubsequencia.length) {
 
-        let ul = document.createElement('ul'); // -> Cria o elemento '<ul>' no documento HTML
-        let maiorSubsequencia = findLongestWord(listaSubsequencia); // -> Procura a palavra mais longa na lista
+        let ul = document.createElement('ul');
+        let maiorSubsequencia = findLongestWord(listaSubsequencia);
 
         resultado.innerHTML += `<p>Subsequências da palavra "${principal.value}":</p>`;
 
-        resultado.appendChild(ul); // -> Adiciona o elemento '<ul>' ao elemento '<div>'
+        resultado.appendChild(ul);
         
-        for (let palavra of listaSubsequencia) { // -> Percorre a lista de palavras
-            
-            let li = document.createElement('li'); // -> Cria um elemento '<li>' no documento HTML
-            let liText = document.createTextNode(palavra); // -> Cria um texto
+        for (let palavra of listaSubsequencia) {
+            /*
+            Percorre o array 'listaSubsequencia', transformando cada palavra em um elemento de lista ('<li>') para
+            uma lista desordenada ('<ul>').
+            */
+            let li = document.createElement('li');
+            let liText = document.createTextNode(palavra);
 
-            li.appendChild(liText); // -> Adiciona o texto ao elemento '<li>'
-            ul.appendChild(li); // -> Adiciona o elemento '<li>' ao elemento '<ul>'
+            li.appendChild(liText);
+            ul.appendChild(li);
         };
         
         resultado.innerHTML += `<p>A subsequência mais longa é ${maiorSubsequencia}.</p>`;
     
-    } else { // -> Apresenta uma mensagem secundária caso não haja subsequências
+    } else {
         resultado.innerHTML = `<p>Nenhuma das palavras indicadas é uma subsequência de "${principal.value}".</p>`;
     };
 };
